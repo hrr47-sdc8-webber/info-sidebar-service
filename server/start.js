2ebee8dfc867e4d23287632f8b0bb93d24aa8692
Josh Rogen <rogenjh@gmail.com>
2020-08-17 17:28:00 -0400

    changed server port, db name, and added a script to add schema

--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
 const app = require('./index.js');
 
 app.listen(3001, () => {
-  console.log('Example app listening at http://localhost:3000');
+  console.log('Example app listening at http://localhost:3001');
 });
