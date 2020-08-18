6e25ff2fd7a78e5f6824185e539b24325ebe5e0c
Josh Rogen <rogenjh@gmail.com>
2020-08-17 22:15:59 -0400

    changed client requests due to port change

--- src/js/components/app.jsx
+++ src/js/components/app.jsx
@@ -46,7 +46,7 @@ class App extends React.Component {
   }
 
   fetchData() {
-    axios.get(`http://localhost:3001/0/restaurants${window.location.pathname}`)
+    axios.get(`http://localhost:3002/0/restaurants${window.location.pathname}`)
       .then((data) => {
         this.setState({
           restaurantID: data.data[0][0].Restaurant_ID,
--- src/js/components/restaurantInfo.jsx
+++ src/js/components/restaurantInfo.jsx
@@ -121,7 +121,7 @@ function RestaurantInfo({
       </div>
       <div className="horizontal-rule" />
       <div className="horizontal-rule" />
-      <img className="zagat-logo" alt="The Zagat Logo" src="http://localhost:3001/imgs/z-logo-icon-red.svg" />
+      <img className="zagat-logo" alt="The Zagat Logo" src="http://localhost:3002/imgs/z-logo-icon-red.svg" />
       <div className="review-text">THE ZAGAT REVIEW</div>
       <Ratings
         foodRating={foodRating}
